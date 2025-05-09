be575b946c1552271aa7e6b526c7aed678e4dc3c
Minwook07 <mith.chett@gmail.com>
2025-05-09 19:51:50 +0700

    add real speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import Aos from 'aos'
 import { IconBase } from 'react-icons'
 import 'aos/dist/aos.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 
 Aos.init()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Analytics />
+    <SpeedInsights />
     <App />
   </StrictMode>,
 )
